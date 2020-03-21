96ac7cbebc5c09eba688ba4019948500575ff7bb
Brijesh026 <brijesh026@gmail.com>
2020-03-22 01:00:53 +0530

    second update with advance faetures

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { formatDate } from "@angular/common";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,59 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'notes-app';
+
+  
+  pastDate = formatDate(new Date(2020,2,20,22,30,20), 'MMMM d, y h:mm:ss a','en-US');
+  notes: string[] = ["                       "+this.pastDate + "\n Notes1"];
+  opened: boolean = true;
+  sidebarStatus ="Hide Sidebar";
+  noteSelected = false;
+  selectedNote: any;
+  index: number;
+  columns: number = 20;
+  
+  ngOnChanges(){
+    if(this.notes.length === 0){
+      this.sidebarStatus = "Show Sidebar";
+      this.columns = 30;
+    }
+    else{
+      this.sidebarStatus = "Hide Sidebar";
+      this.columns = 20;
+    }
+  }
+  
+  addNotes(){
+    this.noteSelected = false;
+    const presentDate = formatDate(new Date(), 'MMMM d, y h:mm:ss a','en-US');
+    this.notes = [`                       ${presentDate}     \n\n`+
+                    `New Note${this.notes.length+1}`, ...this.notes];
+  }
+
+
+
+  deleteNotes(){
+    this.noteSelected = false;
+    this.notes.splice(this.index,1);
+  }
+
+  sidenavToggle(){
+    this.opened = !this.opened;
+    if(this.opened){
+      this.sidebarStatus = "Hide Sidebar";
+      this.columns = 20;
+    }
+    else{
+      this.sidebarStatus = "Show Sidebar";
+      this.columns = 30;
+    }
+  }
+
+  showNotes(note: any){
+    this.noteSelected = true;
+    this.index = this.notes.indexOf(note);
+    note.value = this.notes[this.index];
+    this.notes[this.index] = note;
+  }
+
 }
