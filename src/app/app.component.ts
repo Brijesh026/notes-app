3d7529bb466a19b5afd1595c7c226937f536fe83
Brijesh026 <brijesh026@gmail.com>
2020-03-23 20:59:29 +0530

    implemented search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,43 +9,82 @@ import { formatDate } from "@angular/common";
 
 export class AppComponent {
   title = 'notes-app';
-
   
   pastDate = formatDate(new Date(2020,2,20,22,30,20), 'MMMM d, y h:mm:ss a','en-US');
   
   notes = [
     {
-      title: "Note Title",
+      title: "Title",
       date: this.pastDate,
-      body: "Example Note text ..."
+      body: "Text"
     }
   ];
 
   opened: boolean = true;
   sidebarStatus = "Hide Sidebar";
   noteSelected = false;
-  index: number;
+  index: number = 0;
+  searchDisabled = false;
+  searchPlaceholder = "Search here..";
+  searchFlag = false;
+  searchedNotes: any = [{title:"-", date: "-", body: "-"}];
 
-  constructor(){}
 
+searchNotes(search: any){
+    this.focusIn();
+    let conf = 0;
+    let item: string = search.value;
+    item = item.toLowerCase();
+    let i = 0;
+    while(i < this.notes.length-1){
+      let title: string = this.notes[i].title.toLowerCase();
+      let date: string = this.notes[i].date.toString();
+      date = date.toLowerCase()
+      let body: string = this.notes[i].body.toLowerCase();
+      let s1:number = title.search(item);
+      let s2:number = date.search(item);
+      let s3:number = body.search(item);
+      //console.log(s1,s2,s3);
+      
+      if(s1 > -1 || s2 > -1 || s3 > -1 ){
+        this.searchedNotes = [this.notes[i], ...this.searchedNotes];
+        this.searchFlag = true;
+        conf++;
+      }
+      i++;
+    }
+    
+    if(this.searchFlag){
+      this.searchedNotes.splice((this.searchedNotes.length-1),1);
+    }
+    else {
+      return alert("No records found !!");
+    }
+
+}
 
   addNotes(){
-    this.noteSelected = false;
+    this.searchFlag = false;
+    this.searchPlaceholder = "Search here..";
     const presentDate = formatDate(new Date(), 'MMMM d, y h:mm:ss a','en-US');
     this.notes = [
       {
-        title: `Title New note${this.notes.length+1}` ,
+        title: `Title New note${this.notes.length+1}`,
         date: presentDate,
-        body: "Your note goes here..."
+        body: `Text${this.notes.length+1}`
       }, ...this.notes
     ]
-    if(this.notes.length > 0)
+    if(this.notes.length > 0){
       this.sidebarStatus = "Show Sidebar";
+      this.searchDisabled = false;
+
+    }
   }
 
 
 
   deleteNotes(){
+    this.searchFlag = false;
     if(this.noteSelected){
       this.notes.splice(this.index,1);
       this.noteSelected = false;
@@ -57,6 +96,8 @@ export class AppComponent {
     if(this.notes.length === 0){
       this.opened=false;
       this.sidebarStatus = "Sidebar Disabled";
+      this.searchDisabled = true;
+      this.searchPlaceholder = "Disabled";
     }
   }
 
@@ -77,11 +118,21 @@ export class AppComponent {
   selectNote(note: any){
     this.noteSelected = true;
     this.index = this.notes.indexOf(note);
-    const id = this.index;
+    let id = this.index;
     //console.log(this.index, note.title);
-    this.notes[id].title = note.title;
-    this.notes[id].date = note.date;
-    this.notes[id].body = note.body;
+    
   }
+
+  focusIn(){
+    let c = this.searchedNotes.length-1;
+    while(c>0){
+      this.searchedNotes.splice(c,1);
+      c--;
+   }
+ }
+
+ focusOut(search: any){
+   search.value = "";
+ }
 
 }
