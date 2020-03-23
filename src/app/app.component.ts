6b5193985c9824da879c0eae084214d9cc16b999
Brijesh026 <brijesh026@gmail.com>
2020-03-24 01:37:31 +0530

    improved search and responsivness

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { formatDate } from "@angular/common";
 
 @Component({
@@ -10,23 +10,41 @@ import { formatDate } from "@angular/common";
 export class AppComponent {
   title = 'notes-app';
   
+  @HostListener('window:resize', ['$event']) onResize(event?) {
+    this.innerWidth = window.innerWidth;
+  }
+  public innerWidth: any = window.innerWidth;;
+  
+  ngOnInit() {
+      
+      if(this.innerWidth <= 768){
+        this.opened = false;
+        this.sidebarStatus = "Show Sidebar";
+        this.cols = 79;
+      }
+      if(this.innerWidth > 768){
+        this.opened = true;
+        this.sidebarStatus = "Hide Sidebar";
+        this.cols = 57;
+      }
+   }
+
+   ngAfterViewInit(){
+     this.onResize();
+   }
+  
+
   pastDate = formatDate(new Date(2020,2,20,22,30,20), 'MMMM d, y h:mm:ss a','en-US');
   
-  notes = [
-    {
-      title: "Title",
-      date: this.pastDate,
-      body: "Text"
-    }
-  ];
-
-  opened: boolean = true;
-  sidebarStatus = "Hide Sidebar";
-  noteSelected = false;
+  notes = [{title: "Title", date: this.pastDate, body: "Text"}];
+  cols: number = 57;
   index: number = 0;
-  searchDisabled = false;
-  searchPlaceholder = "Search here..";
-  searchFlag = false;
+  opened: boolean = true;
+  noteSelected: boolean = false;
+  searchDisabled: boolean = false;
+  searchFlag: boolean = false;
+  sidebarStatus: string = "Hide Sidebar";
+  searchPlaceholder: string = "Search here..";
   searchedNotes: any = [{title:"-", date: "-", body: "-"}];
 
 
@@ -36,7 +54,7 @@ searchNotes(search: any){
     let item: string = search.value;
     item = item.toLowerCase();
     let i = 0;
-    while(i < this.notes.length-1){
+    while(i < this.notes.length){
       let title: string = this.notes[i].title.toLowerCase();
       let date: string = this.notes[i].date.toString();
       date = date.toLowerCase()
@@ -44,8 +62,6 @@ searchNotes(search: any){
       let s1:number = title.search(item);
       let s2:number = date.search(item);
       let s3:number = body.search(item);
-      //console.log(s1,s2,s3);
-      
       if(s1 > -1 || s2 > -1 || s3 > -1 ){
         this.searchedNotes = [this.notes[i], ...this.searchedNotes];
         this.searchFlag = true;
@@ -53,14 +69,14 @@ searchNotes(search: any){
       }
       i++;
     }
-    
     if(this.searchFlag){
       this.searchedNotes.splice((this.searchedNotes.length-1),1);
+      this.index = 0;
     }
     else {
+      this.searchFlag = false;
       return alert("No records found !!");
     }
-
 }
 
   addNotes(){
@@ -74,11 +90,12 @@ searchNotes(search: any){
         body: `Text${this.notes.length+1}`
       }, ...this.notes
     ]
-    if(this.notes.length > 0){
+    if(this.notes.length === 1){
       this.sidebarStatus = "Show Sidebar";
       this.searchDisabled = false;
-
     }
+    if(this.notes.length ===1 && this.sidebarStatus === "Show Sidebar")
+      this.cols = 79;
   }
 
 
@@ -102,26 +119,29 @@ searchNotes(search: any){
   }
 
 
+
   sidenavToggle(){
     if(this.notes.length > 0){
       this.opened = !this.opened;
       if(this.opened){
         this.sidebarStatus = "Hide Sidebar";
+        this.cols = 57;
       }
       else{
         this.sidebarStatus = "Show Sidebar";
+        this.cols = 79;
       }
     }
   }
 
   
+
   selectNote(note: any){
     this.noteSelected = true;
     this.index = this.notes.indexOf(note);
     let id = this.index;
-    //console.log(this.index, note.title);
-    
   }
+
 
   focusIn(){
     this.searchFlag = false;
@@ -131,6 +151,7 @@ searchNotes(search: any){
       c--;
    }
  }
+
 
  focusOut(search: any){
    search.value = "";
